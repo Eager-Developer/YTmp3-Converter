e973ea8db377a10c912566d543f905fb2f4ab0c3
Eager-Developer <eager_developer@proton.me>
2022-09-25 22:01:41 +0200

    Modified the way the file system works + changed a bit of syntax + fixed some bugs that could cause an exception

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
 from datetime import datetime
 import os
 import re
-import traceback
 import yt_dlp
 
+def abort():
+    input("END of the program. Please press Enter to exit.")
+    exit()
+    
 #region Constants and global variables
 
 print("--- PLEASE NOTICE - Default value is picked if you press the enter key without typing anything ---\n")
 
-COMMON_WIN_DRIVES = ['C', 'D', 'S', 'E']
-default_win_drive = None
-for COMMON_WIN_DRIVE in reversed(COMMON_WIN_DRIVES):
-    if os.path.exists(COMMON_WIN_DRIVE + ":\\" + "Downloads\\"):
-        default_win_drive = COMMON_WIN_DRIVE
-DEFAULT_WIN_DRIVE = default_win_drive + ":\\"
-
+# constants    
 DEFAULT_OUTPUT_FOLDER_NAME = "MP3 conversion result"
-output_folder_name = input("Output folder name [DEFAULT = \"" + DEFAULT_OUTPUT_FOLDER_NAME + "\"]: ").strip()
-if not(output_folder_name):
-    output_folder_name = DEFAULT_OUTPUT_FOLDER_NAME
-OUTPUT_FOLDER_NAME = output_folder_name + "\\"
 
-if (not(DEFAULT_WIN_DRIVE)):
-    output_base_dir_path = input("Output folder path (i.e. \"C:\\Downloads\\\"): ").strip()
-else:
-    DEFAULT_OUTPUT_BASE_DIR_PATH = "./"#DEFAULT_WIN_DRIVE + "Downloads\\YTmp3\\"
-    output_base_dir_path = input("Output folder path [DEFAULT = \"" + DEFAULT_OUTPUT_BASE_DIR_PATH + "\"]: ").strip()
-    if not(output_base_dir_path):
-        output_base_dir_path = DEFAULT_OUTPUT_BASE_DIR_PATH
+DEFAULT_OUTPUT_BASE_DIR_PATH = "./"
+output_base_dir_path = input("Output folder path (DEFAULT = \"" + DEFAULT_OUTPUT_BASE_DIR_PATH + "\" (current directory)): ").strip()
+if not output_base_dir_path:
+    output_base_dir_path = DEFAULT_OUTPUT_BASE_DIR_PATH
 OUTPUT_BASE_DIR_PATH = output_base_dir_path
-
-OUTPUT_FOLDER_PATH = OUTPUT_BASE_DIR_PATH + OUTPUT_FOLDER_NAME
+OUTPUT_FOLDER_PATH = OUTPUT_BASE_DIR_PATH + "\\" + DEFAULT_OUTPUT_FOLDER_NAME
+if not os.path.isdir(OUTPUT_FOLDER_PATH):
+    os.mkdir(OUTPUT_FOLDER_PATH)
 
 DEFAULT_LINKS_FILE_NAME = "links.txt"
-links_file_name = input("File name for URL list [DEFAULT = \"" + DEFAULT_LINKS_FILE_NAME + "\"]: ").strip()
+links_file_name = input("File name for URL list (DEFAULT = \"" + DEFAULT_LINKS_FILE_NAME + "\"): ").strip()
 if not links_file_name:
     links_file_name = DEFAULT_LINKS_FILE_NAME
 LINKS_FILE_NAME = links_file_name
-
 LINKS_FILE_PATH = OUTPUT_BASE_DIR_PATH + LINKS_FILE_NAME
+if not os.path.exists(LINKS_FILE_PATH):
+    f = open(LINKS_FILE_PATH, "w")
+    f.close()
+    print("FileNotFoundError: Please create a file to store your YT links (DEFAULT = \"" + DEFAULT_LINKS_FILE_NAME + "\").")
+    abort()
 
-# If there is no folder, create it
-if (not(os.path.isdir(OUTPUT_FOLDER_PATH))):
-    os.mkdir(OUTPUT_FOLDER_PATH)
+UTILITY_FOLDER = "Utility\\"
+UTILITY_FOLDER_PATH = DEFAULT_OUTPUT_BASE_DIR_PATH + UTILITY_FOLDER
+if not os.path.isdir(UTILITY_FOLDER_PATH):
+    os.mkdir(UTILITY_FOLDER_PATH)
 
 # youtube-dl changes some special characters to underscore: for comparison purposes, we store them in a list
 CHAR_TO_CHANGE_LIST = {
@@ -53,6 +49,7 @@ CHAR_TO_CHANGE_LIST = {
     '"': '\''
 }
 
+# global variables
 equivalent_mp3_counter = 0
 error_links = []
 
@@ -75,22 +72,22 @@ def get_mp3(yt_url):
         "keepvideo": False,
         "outtmpl": OUTPUT_FOLDER_PATH + f"{'%(title)s'}.mp3",
     }
-
-    mp3_file = OUTPUT_FOLDER_PATH + video_title + ".mp3"
-    mp3_file_revised = ""
-    for char in mp3_file:
+    
+    revised_video_title = ""
+    for char in video_title:
         for key, value in CHAR_TO_CHANGE_LIST.items():
             if char == key:
                 char = value
-        mp3_file_revised += char
+        revised_video_title += char
+    mp3_file = OUTPUT_FOLDER_PATH + revised_video_title + ".mp3"
 
-    is_mp3_equivalent_existing = os.path.exists(mp3_file_revised)
+    is_mp3_equivalent_existing = os.path.exists(mp3_file)
     with yt_dlp.YoutubeDL(options) as ydl:
         if not is_mp3_equivalent_existing:
             try:
                 ydl.download([video_info["webpage_url"]])
-            except Exception:
-                print(traceback.format_exc())
+            except Exception as e:
+                print(e)
         else:
             global equivalent_mp3_counter
             equivalent_mp3_counter += 1
@@ -103,17 +100,15 @@ def get_mp3(yt_url):
 def main():
     global error_links
     url_set = set()
-    try:
-        if os.stat(LINKS_FILE_PATH).st_size != 0:
-            with open(LINKS_FILE_PATH, "r+") as url_file:
-                lines = url_file.readlines()
-                for line in lines:
-                    if not line.isspace() and line not in url_set:
-                        url_set.add(line)
-        else:
-            print("WARNING - Your links file is empty, hence not containing any link to convert.")
-    except FileNotFoundError:
-        print(traceback.format_exc() + "^^^^ Please create a file to store your YT links (DEFAULT = \"" + DEFAULT_LINKS_FILE_NAME + "\")")
+    if os.stat(LINKS_FILE_PATH).st_size != 0:
+        with open(LINKS_FILE_PATH, "r+") as url_file:
+            lines = url_file.readlines()
+            for line in lines:
+                if not line.isspace() and line not in url_set:
+                    url_set.add(line)
+    else:
+        print("WARNING - Your links file is empty, hence not containing any link to convert.")
+        
 
     is_program_reexecuting = True
     while is_program_reexecuting:
@@ -142,12 +137,13 @@ def main():
         else:
             is_program_reexecuting = False
     
+    FILENAME_PREFIX = UTILITY_FOLDER_PATH + re.sub("[':', ' ', '\-', '.']", "", str(datetime.now()))[:-6]
     if len(error_links):
-        file = "utility\\" + re.sub("[':', ' ', '\-', '.']", "", str(datetime.now()))[:-6] + " Problematic URL list.txt"
+        file = FILENAME_PREFIX + " Problematic URL list.txt"
         with open(file, "w") as error_links_file:
             error_links_file.write("".join([url for url in error_links]))
     else:
-        file = "utility\\" + re.sub("[':', ' ', '\-', '.']", "", str(datetime.now()))[:-6] + " SAVE links.txt"
+        file = FILENAME_PREFIX + " SAVE links.txt"
         with open(LINKS_FILE_NAME, "r+") as links_txt, open(file,"w") as save_links_txt:
             for line in links_txt:
                 save_links_txt.write(line)
